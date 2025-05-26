9e1f4680353edda8343ad428f8a7ce5eed5b1838
raiane-h84 <raianechz@gmail.com>
2025-05-26 19:56:57 -0300

    Add files via upload

---
+++ trabalho JS/Exec.js
@@ -0,0 +1,190 @@
+// let a = 8;
+// let b = 4;
+// soma = a + b;
+// console.log(soma);
+
+// // //////////////////////
+// // let nome ="Raiane";
+// console.log(nome);
+// // ////////////////
+// let valor =true;
+// console.log(valor);
+
+// // ///////////////////
+// let pi = 3.1415;
+// console.log(pi);
+// // ///////////////////
+
+// let nomeUsario = prompt("Digite seu nome");
+// let idade = prompt("Digite sua idade");
+// let bairro = prompt("Digite o bairro onde mora");
+// let nomeCompleto = nomeUsario.toUpperCase()
+// alert("Ola, "  + nomeCompleto)
+// console.log(nomeCompleto);
+// // ////////////////////
+// let um = prompt("Digite um número");
+// let dois = prompt("Digite outro número");
+// let somaEles= um + dois;
+// console.log(somaEles);
+
+// //////////////////////
+// let nome = "Raiane machadinha";
+// let sobrenome = nome.replace('machadinha','Heinzmann');
+// console.log(sobrenome);
+// //////////////////////
+
+// let preço = 50;
+// let desconto = 20/100;
+// let preçoComDesconto = preço - (preço * desconto);
+// console.log(preçoComDesconto);
+// ////////////////////
+
+// let idade = 93
+
+// if (idade < 18) {
+//     console.log("Menor de idade")
+// }
+// else if (idade >= 18 && idade < 60) {
+//     console.log("Adulto")
+// }
+
+// else {
+//     console.log("Idoso")
+// }
+///////////////////////////
+// let idade = 32;
+// if (idadeDirigir < 18) {
+//     console.log("Você pode dirigir");
+// }
+// else if (idadeDirigir <= 18) {
+//     console.log("você pode dirigir");
+// }
+// else {
+//     console.log("ingresso Idoso -- Você pagará 15 reais");
+// }
+// //////////////////////////////
+// let numero = parseInt(prompt("Digite um número inteiro:"));
+
+// if (numero > 0) {
+//   console.log(" número positivo.");
+// } else if (numero < 0) {
+//   console.log("número negativo.");
+// } else {
+//   console.log(" número zero.");
+// }
+// let altura = parseFloat(prompt("Altura:"))
+// let peso = parseFloat(prompt("Peso:"))
+// let imc = peso/(altura**2)
+
+// if (imc>= 18.5 && imc<=24.9) {
+//     alert("IMC Dentro do padrão")
+// }
+// else {
+//     alert("IMC fora do padrão")
+// }
+/////////////////////////////////////////////
+// let dia = 15; // USAR SÓ QUANDO DOR UMA VARIAVEL COM MUITAS CONDIÇÕES 
+// switch (dia) {
+//     case 1:
+//         console.log("Hoje é segunda-feira");
+//         break;
+//     case 2:
+//         console.log("Hoje é terça-feira");
+//         break;
+//     case 3:
+//         console.log("Hoje é quarta-feira");
+//         break;
+//     case 4:
+//         console.log("Hoje é quinta-feira");
+//         break;
+//     case 5:
+//         console.log("Hoje é sexta-feira");
+//         break;
+//     case 6:
+//         console.log("Hoje é sábado");
+//         break;
+//     case 7:
+//         console.log("Hoje é domingo");
+//         break;
+//     default:
+//         console.log("Dia inválido");
+//         break;}
+///////////////////
+// let idade = 88;
+// if (idade < 12) {
+//     console.log("Criança");
+// }
+// else if (idade < 18) {
+//     console.log("Asolescente");
+// }
+// else if (idade < 60) {
+//     console.log("Adulto");
+// }
+// else {
+//     console.log("Idoso");
+// }
+///////////////////////////////////////
+// let aa = 8
+// let bb = 4 
+
+// let soma = aa + bb
+// let sub = aa - bb
+// let multi = aa * bb
+// let div = aa / bb 
+
+// let Calculadora = prompt('Qual operação deseja fazer? ')
+
+// switch (Calculadora) {
+//     case "soma":
+//         alert(`A soma é ${soma}`)
+//         break;
+
+//     case "subtração":
+//         alert(`A subtração desses número é ${sub}`)
+//         break;
+
+//     case "multiplicação":
+//         alert(`A multiplicação desses número é ${multi}`)
+//         break;
+
+//     case "divisão":
+//         alert(`A divisão desses número é ${div}`)
+//         break;
+////////////////////////////////////////////////
+// let dinheiro = prompt(" DESC1 - 05 % de desconto \n DESC2 - 10 % de desconto \n DESC3 - 15 % de desconto \n DESC4 - 20 % de desconto \n DESC5 - 25 % de desconto \n Insira o código de desconto: ")
+// let produto = 1000
+
+
+// let desc01 = produto - (produto * 0.05)
+// let desc02 = produto - (produto * 0.10)
+// let desc03 = produto - (produto * 0.15)
+// let desc04 = produto - (produto * 0.20)
+// let desc05 = produto - (produto * 0.25)
+
+// switch (dinheiro) {
+//     case "DESC1":
+//         alert("05 % de desconto")
+//         alert(`Valor original: ${produto} \n Valor c/ desconto: ${desc01}`)
+//         break;
+
+//     case "DESC2":
+//         alert("10 % de desconto")
+//         alert(`Valor original: ${produto} \n Valor c/ desconto: ${desc02}`)
+//         break;
+
+//     case "DESC3":
+//         alert("15 % de desconto")
+//         alert(`Valor original: ${produto} \n Valor c/ desconto: ${desc03}`)
+//         break;
+
+//     case "DESC4":
+//         alert("20 % de desconto")
+//         alert(`Valor original: ${produto} \n Valor c/ desconto: ${desc04}`)
+//         break;
+
+//     case "DESC5":
+//         alert("25 % de desconto")
+//         alert(`Valor original: ${produto} \n Valor c/ desconto: ${desc05}`)
+//         break;
+// }
+
